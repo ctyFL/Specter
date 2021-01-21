66e4cdf8e3e93d2792b56d9b073d8beff148dd51
ctyFL <502651020@qq.com>
2021-01-21 20:42:25 +0800

    使用Thumbnailator图片处理和封装Util

--- o2o/src/main/java/com/ctyFL/o2o/util/ImageUtil.java
+++ o2o/src/main/java/com/ctyFL/o2o/util/ImageUtil.java
@@ -67,7 +67,7 @@ public class ImageUtil {
 		File watermarkImgFile = new File(classPath + "/image/icon/icon_fl.png");
 		try {
 			Thumbnails.of(file)//Thumbnailator图片处理工具类的主类.读取要处理的图片
-			.size(200, 200)//大小：长、宽，单位：像素
+			.size(1628, 762)//大小：长、宽，单位：像素（即要生成的图片的长、宽的像素）
 			.watermark(Positions.BOTTOM_RIGHT, ImageIO.read(watermarkImgFile), 0.25f)//水印：Positiion.BOTTOM_RIGHT——位置右下角，ImageIO.read——读取水印图片的文件流，0.25f——透明度为0.25
 			.outputQuality(0.8f)//压缩图片：80%
 			.toFile(saveFile);//输出路径：输出到保存文件的目标路径
